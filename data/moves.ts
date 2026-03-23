e4be4af80ed668be624474334bf87171d433f4e5
Ian Schmid <2njs6902@gmail.com>
2026-03-22 23:52:05 -0400

    Scraping mod and directly editing server files instead

--- data/moves.ts
+++ data/moves.ts
@@ -1,6 +1,45 @@
 // List of flags and their descriptions can be found in sim/dex-moves.ts
 
 export const Moves: import('../sim/dex-moves').MoveDataTable = {
+	irritation: {
+		num: 6902,
+		accuracy: 100,
+		basePower: 60,
+		basePowerCallback(pokemon, target, move) {
+			if (target.status || target.hasAbility('comatose')) {
+				this.debug('BP doubled from status condition');
+				return move.basePower * 2;
+			}
+			return move.basePower;
+		},
+		category: "Special",
+		name: "Irritation",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1 },
+		secondary: null,
+		target: "normal",
+		type: "Bug",
+		zMove: { basePower: 160 },
+		contestType: "Clever",
+	},	
+	slashandburn: {
+		num: 6903,
+		accuracy: 90,
+		basePower: 90,
+		category: "Physical",
+		name: "Slash And Burn",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1 },
+		secondary: {
+			chance: 10,
+			status: 'brn',
+		},
+		target: "normal",
+		type: "Grass",
+		contestType: "Cute",
+	},
 	"10000000voltthunderbolt": {
 		num: 719,
 		accuracy: true,
