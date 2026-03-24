048844bd7d4a6738b836775725eef13ad8fa7ac0
Ian Schmid <2njs6902@gmail.com>
2026-03-24 02:48:27 -0400

    Attempt 2 to legalizing custom moves

--- config/formats.ts
+++ config/formats.ts
@@ -25,10 +25,10 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		section: "Rejuvenation",
 	},
 	{
-		name: "[Gen 8] NatDex Rejuvenation",
+		name: "[Gen 8] Rejuvenation",
 		desc: `Unofficial Rejuvenation Mod for Pokemon Showdown`,
 		mod: 'rejuvenation',
-		ruleset: ['Standard NatDex'],
+		ruleset: ['NatDex Mod'],
 		banlist: [
 			'Slowpoke-Galar', 'Slowbro-Galar', 'Magikarp', 'Gyarados', 'Articuno', 'Articuno-Galar', 'Zapdos', 'Zapdos-Galar',
 			'Moltres', 'Moltres-Galar', 'Dratini', 'Dragonair', 'Dragonite', 'Mewtwo', 'Mew', 'Slowking-Galar', 'Raikou', 'Entei',
