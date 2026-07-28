458f7efb74e8c0ec85ddc81373fd8719fdc04348
Ian Schmid <150126860+2njs6902@users.noreply.github.com>
2026-07-28 18:13:38 -0400

    Forgot to add items too format

--- data/mods/rejuvenation/items.ts
+++ data/mods/rejuvenation/items.ts
@@ -1,3 +1,26 @@
-export const Items: {[itemid: string]: ModdedItemData} = {
-
+export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
+	absorbbulb: {
+		inherit: true,
+	},
+	adamancrystal: {
+		inherit: true,
+	},
+	adamantorb: {
+		inherit: true,
+	},
+	venusaurite: {
+		inherit: true,
+	},
+	venusauriteg: {
+		inherit: true,
+		name: "Venusaurite G",
+		spritenum: 608,
+		megaStone: { "Venusaur": "Venusaur-Giga" },
+		itemUser: ["Venusaur"],
+		onTakeItem(item, source) {
+			return !item.megaStone?.[source.baseSpecies.baseSpecies];
+		},
+		num: -1000,
+		gen: 9,
+	},
 }
