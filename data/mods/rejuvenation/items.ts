fde68e953c428ef5141587e5345d57535c1e9ebb
Ian Schmid <150126860+2njs6902@users.noreply.github.com>
2026-07-28 19:45:17 -0400

    Update items.ts

--- data/mods/rejuvenation/items.ts
+++ data/mods/rejuvenation/items.ts
@@ -9,27 +9,12 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 		inherit: true,
 	},
 	venusaurite: {
-		name: "Venusaurite",
-		spritenum: 608,
-		megaStone: { "Venusaur": "Venusaur-Mega" },
-		itemUser: ["Venusaur"],
-		onTakeItem(item, source) {
-			return !item.megaStone?.[source.baseSpecies.baseSpecies];
-		},
-		num: 659,
+		inherit: true,
 		gen: 9,
 		isNonstandard: null,
 	},
 	venusauriteg: {
-		name: "Venusaurite G",
-		spritenum: 608,
-		megaStone: { "Venusaur": "Venusaur-Giga" },
-		itemUser: ["Venusaur"],
-		onTakeItem(item, source) {
-			return !item.megaStone?.[source.baseSpecies.baseSpecies];
-		},
-		num: 6902,
-		gen: 9,
+		inherit: true,
 		isNonstandard: null,
 	},
 }
